646b966aa4f21da2cfc3457aba28d7b137a879ed
Tetiana Nieizviestna <viv.tnn.x@gmail.com>
2021-03-19 20:22:00 +0200

    Project init, Pod init

---
+++ ViVNewsApp/ViVNewsApp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  ViVNewsApp
+//
+//  Created by Tetiana Nieizviestna on 19.03.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ ViVNewsApp/ViVNewsApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ViVNewsApp
+//
+//  Created by Tetiana Nieizviestna on 19.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
